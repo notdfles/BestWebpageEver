5d3bc900974ed928e42a6200bfe6b7fb44ea0540
Michael Schiff <schiff.mi@gmail.com>
2014-08-22 14:26:52 -0400

    All done

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title> We Da Best </title>
+</head>
+<body>
+  <h1> We Da Best </h1>
+  <h2> <marquee><blink>Knew it when we started, know it when we leave. </blink></marquee></h2>
+  <h3> We out, suckahs. </h2>
+</body>
+</html>
